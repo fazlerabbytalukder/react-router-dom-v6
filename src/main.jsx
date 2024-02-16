fc6c52cccadfc0d345ce38d7c18010fbda10065c
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2024-02-16 15:02:12 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Error from './Error';
+import Root from './Root';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <Error />
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
