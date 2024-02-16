f8ad220c5c40e7820c6a0d194b4095e3b4261a5f
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2024-02-16 20:17:46 +0600

    add edit delete

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,4 +1,8 @@
+import { Form, NavLink, Outlet, useLoaderData, useNavigation } from "react-router-dom";
+
 export default function Root() {
+    const { contacts } = useLoaderData();
+    const navigation = useNavigation();
     return (
         <>
             <div id="sidebar">
@@ -22,22 +26,49 @@ export default function Root() {
                             aria-live="polite"
                         ></div>
                     </form>
-                    <form method="post">
+                    <Form method="post">
                         <button type="submit">New</button>
-                    </form>
+                    </Form>
                 </div>
                 <nav>
-                    <ul>
-                        <li>
-                            <a href={`/contacts/1`}>Your Name</a>
-                        </li>
-                        <li>
-                            <a href={`/contacts/2`}>Your Friend</a>
-                        </li>
-                    </ul>
+                    {contacts.length ? (
+                        <ul>
+                            {contacts.map((contact) => (
+                                <li key={contact.id}>
+                                    <NavLink
+                                        to={`contacts/${contact.id}`}
+                                        className={({ isActive, isPending }) =>
+                                            isActive
+                                                ? "active"
+                                                : isPending
+                                                    ? "pending"
+                                                    : ""
+                                        }
+                                    >
+                                        {contact.first || contact.last ? (
+                                            <>
+                                                {contact.first} {contact.last}
+                                            </>
+                                        ) : (
+                                            <i>No Name</i>
+                                        )}{" "}
+                                        {contact.favorite && <span>★</span>}
+                                    </NavLink>
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p>
+                            <i>No contacts</i>
+                        </p>
+                    )}
                 </nav>
             </div>
-            <div id="detail"></div>
+            <div id="detail" className={
+                navigation.state === "loading" ? "loading" : ""
+            }>
+                <Outlet />
+            </div>
         </>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Contact from './Contacts';
+import EditContacts from './EditContacts';
 import Error from './Error';
 import Root from './Root';
+import { createContactAction, deleteContactAction, editContactAction } from './actions/contactsAction';
 import './index.css';
+import { getContactloader, getContactsoader } from './loaders/contactsLoader';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
-    errorElement: <Error />
-  }
+    errorElement: <Error />,
+    loader: getContactsoader,
+    action: createContactAction,
+    children: [
+      {
+        path: '/contacts/:contactId',
+        element: <Contact />,
+        loader: getContactloader,
+      },
+      {
+        path: '/contacts/:contactId/edit',
+        element: <EditContacts />,
+        loader: getContactloader,
+        action: editContactAction,
+      },
+      {
+        path: '/contacts/:contactId/destroy',
+        action: deleteContactAction,
+        errorElement: <div>Oops! There was an error.</div>,
+      }
+    ]
+  },
+
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
